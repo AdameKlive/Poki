78597c69606ba4d4bedc5d7c855254bf11341752
AdameK <153874504+AdameKlive@users.noreply.github.com>
2025-06-01 18:06:17 +0200

    Add files via upload

--- serwer/data/spells/scripts/moves/electric/thunder.lua
+++ serwer/data/spells/scripts/moves/electric/thunder.lua
@@ -8,37 +8,51 @@ table.insert(area, createCombatArea(LeafStorm3))
 table.insert(area, createCombatArea(LeafStorm4))
 
 local areaDamage = {}
-table.insert(areaDamage, createCombatArea(AREA_CIRCLE5X5))
-table.insert(areaDamage, createCombatArea(AREA_CIRCLE5X5))
-table.insert(areaDamage, createCombatArea(AREA_CIRCLE5X5))
-table.insert(areaDamage, createCombatArea(AREA_CIRCLE5X5))
-
-local effect = {}
-table.insert(effect, 772)
-table.insert(effect, 772)
-table.insert(effect, 772)
-table.insert(effect, 772)
-
-local damageMultiplier = damageMultiplierMoves.ultimate/#area
-
-local function spellCallback(uid, position, damage, count)
-	local creature = Creature(uid)
-	if creature then		
-		if count <= #area then
-			if not effect[count] or not areaDamage[count] then return true end
-			doAreaCombatHealth(uid, combat, position, area[count], 0, 0, effect[count])
-			doAreaCombatHealth(uid, combat, position, areaDamage[count], -damage, -damage, 0)
-			count = count + 1			
-			addEvent(spellCallback, time, uid, position, damage, count)
-		end
-	end
+for i = 1, 4 do
+    table.insert(areaDamage, createCombatArea(AREA_CIRCLE5X5))
 end
 
+local effect = {772, 772, 772, 772}
+
+local damageMultiplier = damageMultiplierMoves.ultimate / #area
+
+-- Główna funkcja działania skilla
+local function spellCallback(uid, position, damage, count)
+    local creature = Creature(uid)
+    if creature then		
+        if count <= #area then
+            if not effect[count] or not areaDamage[count] then return true end
+            doAreaCombatHealth(uid, combat, position, area[count], 0, 0, effect[count])
+            doAreaCombatHealth(uid, combat, position, areaDamage[count], -damage, -damage, 0)
+            count = count + 1			
+            addEvent(spellCallback, time, uid, position, damage, count)
+        end
+    end
+end
+
+-- Główna funkcja rzucania czaru
 function onCastSpell(creature, variant)
+    local player = Player(creature:getMaster()) -- pobierz trenera
+    local pokeName = creature:getName():lower()
+    local spellName = "thunder"
 
-	local damage = damageMultiplier * creature:getTotalMagicAttack()
-	local position = creature:getPosition()
+    -- Sprawdź poziom wymagany z pokehabilidades
+    local minLevel = SkillLock[pokeName] and SkillLock[pokeName][spellName]
+    if not minLevel then
+        player:sendTextMessage(MESSAGE_STATUS_SMALL, "Ten atak nie jest dostępny dla " .. pokeName .. ".")
+        return false
+    end
 
-	spellCallback(creature.uid, position, damage, 1)
-	return true
+    if creature:getLevel() < minLevel then
+        player:sendTextMessage(MESSAGE_STATUS_SMALL, pokeName .. " potrzebuje przynajmniej poziomu " .. minLevel .. ", aby użyć " .. spellName .. ".")
+        
+        -- Wyłącz animację cooldownu tylko dla tego skilla
+    end
+
+    -- Oblicz obrażenia i rozpocznij atak
+    local damage = damageMultiplier * creature:getTotalMagicAttack()
+    local position = creature:getPosition()
+    spellCallback(creature.uid, position, damage, 1)
+
+    return true
 end
